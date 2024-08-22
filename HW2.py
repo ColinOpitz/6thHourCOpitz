9eca4eb451d44815a4f4656f10abc5f7df12a6c9
Colin Opitz <co124201@student.wpsok.org>
2024-08-22 13:38:14 -0500

    Added Comments, Hello World print, Name input, and number input.

---
+++ HW2.py
@@ -0,0 +1,22 @@
+#Name: Colin Opitz
+#Class: 6th Hour
+#Assignment: HW2
+
+#Print Hello World
+
+#Make an input that asks for your name
+
+#Make an input that asks for two integers
+
+#Add those two integers together and print them
+
+print("Hello World")
+
+name = input("What is your name? ")
+print("Hello",name)
+
+x = int(input("Give me a number: "))
+y = int(input("Give me another number: "))
+z = x + y
+print(z)
+
