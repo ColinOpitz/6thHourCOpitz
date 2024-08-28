ca4c1df1f916626edfd7e777817154ca85b8e706
Colin Opitz <co124201@student.wpsok.org>
2024-08-28 14:02:20 -0500

    Added Comments, Hello World print, Name list, and number list.

---
+++ HW3.py
@@ -0,0 +1,21 @@
+#Name : Colin Opitz
+#Class: 6th Hour
+#Assignment: HW3
+
+
+print("Hello World")
+
+namesList = ["Colin","Ryan","CJ","Bowser","Knuckles"]
+
+print(f"{namesList[3]} is Cool!")
+
+namesList.append("Mario")
+
+numList = [0,1,2,3]
+numList.insert(1, 7)
+print(numList)
+
+numList.sort()
+print(numList)
+sum = numList[0] + numList[1] + numList[2]
+print(sum)
