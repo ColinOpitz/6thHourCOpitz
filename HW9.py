d74249e14b156a2f796c8d08f252fbe2e5566b05
Colin Opitz <co124201@student.wpsok.org>
2024-10-24 13:52:57 -0500

    Added temperature variable and if statements.

---
+++ HW9.py
@@ -0,0 +1,18 @@
+#Name: COlin Opitz
+#Class: 6th Hour
+#Assignment: HW9
+
+#1. Print Hello World!
+
+#2. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
+#https://adacomputerscience.org/images/content/computer_science/design_and_development/program_design/figures/ada_cs_design_flow_ifelseif.svg
+temperature = int(input("What's the temperature? "))
+
+if temperature > 20:
+    print("It's hot")
+elif temperature >10:
+        print("It's mild")
+else:
+        print("It's cold")
+
+print("Thank You!")
