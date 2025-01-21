d07823eb10761b363e02e07c40c29837b0d5454d
Colin Opitz <co124201@student.wpsok.org>
2025-01-21 14:06:43 -0600

    Added HW15 Code

---
+++ HW17.py
@@ -0,0 +1,33 @@
+#Name: Colin Opitz
+#Class: 6th Hour
+#Assignment: HW17
+
+import random
+
+#1. Import the "random" library and create a def function that prints "Hello World!"
+def hello_world():
+    print("Hello World!")
+#2. Create two empty integer variables named "heads" and "tails"
+
+#3. Create a def function that flips a coin one hundred times and increments the result in the above variables.
+def flip_coin():
+    heads = 0
+    tails = 0
+
+    for _ in range(100):
+        result = random.choice(["heads", "tails"])
+        if result == "heads":
+            heads += 1
+        else:
+            tails += 1
+
+    return heads, tails
+
+heads_count, tails_count = flip_coin()
+
+#4. Call the "Hello world" and "Coin Flip" functions here
+hello_world()
+flip_coin()
+#5. Print the final result of heads and tails here
+print("Heads:", heads_count)
+print("Tails:", tails_count)
