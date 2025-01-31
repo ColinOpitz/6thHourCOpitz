00b23be424877c6d0b0be334204e43277604f065
Colin Opitz <co124201@student.wpsok.org>
2025-01-31 13:51:07 -0600

    Added HW19 code and changed Hw15 code

--- HW15.py
+++ HW15.py
@@ -7,36 +7,25 @@ def hello_world():
     print("Hello World!")
 
 #2. Create a def function that calculates the average of three numbers.
-def average_of_three(num1, num2, num3):
-    return (num1 + num2 + num3) / 3
-a = 10
-b = 20
-c = 30
-result = average_of_three(a, b, c)
-print("The average is:", result)
+def average_of_three(a, b, c):
+    print((a + b + c) / 3)
+
+
+
 #3. Create a def function with the names of 5 animals as arguments, treats it like a list, and
 #prints the name of the third animal.
 def animal_name(*animal):
     print("The third animal's name is", animal[2])
 
-animal_name("Badger", "Lion", "Snake", "Raven", "Dog")
+
 
 #4. Create a def function that loops from 1 to the number put in the argument.
-numInput = int(input("Pick a number: "))
-def number_game(numInput):
-    for i in range(1, numInput + 1):
+def number_game(x):
+    i = 1
+
+    while i <=x:
         print(i)
-
-number_game(numInput)
-if number_game == numInput:
-    number_game(numInput)
-else:
-    exit()
-
-number_game(numInput)
+        i+=1
 #5. Call all of the functions created in 1 - 4 with relevant arguments.
-hello_world()
-average_of_three()
-animal_name()
-number_game()
+
 
---
+++ HW19.py
@@ -0,0 +1,12 @@
+#Name: Colin Opitz
+#Class: 6th Hour
+#Assignment: HW19
+
+#1. Import all the functions from the HW15 file.
+from HW15 import *
+#2. Call the functions here and run HW19.
+hello_world()
+average_of_three(10, 20, 30)
+animal_name("Badger", "Lion", "Snake", "Raven", "Dog")
+number_game(5)
+#3. Delete all calls from HW15 and run HW19 again.
